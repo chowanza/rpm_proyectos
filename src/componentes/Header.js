002926e5ca89d5639f93b873964f3c7cdebbc104
chowanza <luiscandialesfajardo@gmail.com>
2024-12-12 22:10:02 -0400

    Added Header as component + hamburguer Menu

---
+++ src/componentes/Header.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Header.css'; // Importa los estilos CSS
+import logo from './img/logo.png';
+
+const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <header className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
+        <a href="#home">Inicio</a>
+        <a href="#nosotros">Nosotros</a>
+        <a href="#obras">Obras</a>
+        <a href="#clientes">Clientes</a>
+        <a href="#contacto">Contacto</a>
+      </nav>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+    </header>
+  );
+}
+
+export default Header;
